99326f39688c646319ff8f0bc90e0bdf7e626071
yutong pang <yutongpang@yutongs-iMac.local>
2015-03-20 00:51:57 -0400

    rawfile_process

--- EBICanalysis/rawfileprocess.py
+++ EBICanalysis/rawfileprocess.py
@@ -1,8 +1,24 @@
 __author__ = 'yutongpang'
 from os import stat
+from array import array
+from sys import byteorder as system_endian
 
 
 class Rawfile():
-    def get_file_stat_count(self, filename):
+    def get_file_data(self, filename, endian):
+        with open(filename, 'rb') as file:
+            result = array('h')
+            result.fromfile(file,self.get_file_stat_count(filename))
+            result = self.process_data_based_on_endian(result, endian)
+        return result
+
+    @staticmethod
+    def process_data_based_on_endian(array, endian):
+        if endian != system_endian:
+            array.byteswap()
+        return array
+
+    @staticmethod
+    def get_file_stat_count(filename):
         file_size_count = int(stat(filename).st_size / 2)
         return file_size_count
